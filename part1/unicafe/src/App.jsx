d0423bebffa88d1172000fd418c1b493112de6d5
Luiz Lopes <luizclopes.soares@gmail.com>
2025-01-17 09:26:41 -0300

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,10 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all;
   const positive = (good / all) * 100;
 
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  }
+
   return (
     <div>
       <h2>statistics</h2>
