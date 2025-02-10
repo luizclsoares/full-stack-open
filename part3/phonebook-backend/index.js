a4729641e7cdc64ee8f6b5207da4043ffb253b38
Luiz Lopes <luizclopes.soares@gmail.com>
2025-02-10 00:30:04 -0300

    3.17*: Phonebook database, step 5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -103,6 +103,21 @@ app.post("/api/persons", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((err) => next(err));
+});
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
