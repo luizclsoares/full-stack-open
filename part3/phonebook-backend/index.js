929793bdeb09de48f0743c14bc2a1084b85fc747
Luiz Lopes <luizclopes.soares@gmail.com>
2025-02-14 03:30:21 -0300

    3.22: Lint configuration

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -52,7 +52,7 @@ app.get("/api/persons/:id", (req, res, next) => {
 
 app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then((result) => {
+    .then(() => {
       res.status(204).end();
     })
     .catch((err) => next(err));
