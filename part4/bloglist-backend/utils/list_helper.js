5e3ef48867c2c119111beee2bd0676d641d09895
Luiz Lopes <luizclopes.soares@gmail.com>
2025-02-18 02:52:08 -0300

    4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -3,6 +3,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) =>
+  blogs.reduce((sum, item) => (sum += item.likes), 0);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
