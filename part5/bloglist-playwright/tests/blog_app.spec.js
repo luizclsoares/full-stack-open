9e6d12afa8c2d233dedc9c6bd485bf873ed2ee61
Luiz Lopes <luizclopes.soares@gmail.com>
2025-03-28 02:04:46 -0300

    5.18: Blog List End To End Testing, step 2

--- part5/bloglist-playwright/tests/blog_app.spec.js
+++ part5/bloglist-playwright/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "Luiz Lopes",
+        username: "luizclsoares",
+        password: "password",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
@@ -9,5 +18,23 @@ describe("Blog app", () => {
     await expect(page.getByTestId("username")).toBeVisible();
     await expect(page.getByTestId("password")).toBeVisible();
     await expect(page.getByRole("button", { name: "Submit" })).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("luizclsoares");
+      await page.getByTestId("password").fill("password");
+      await page.getByRole("button", { name: "Submit" }).click();
+      await expect(page.getByText("Luiz Lopes logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("luizclsoares");
+      await page.getByTestId("password").fill("passwordd");
+      await page.getByRole("button", { name: "Submit" }).click();
+
+      await expect(page.getByText("Wrong username or password")).toBeVisible();
+      await expect(page.getByText("Luiz Lopes logged in")).not.toBeVisible();
+    });
   });
 });
