4ea1dca7cad91a0f49575e07e88bd455be581c3d
Luiz Lopes <luizclopes.soares@gmail.com>
2026-04-25 05:13:41 -0300

    6.9 anecdotes, step8

--- part6/zustand-anecdotes/src/services/anecdotes.js
+++ part6/zustand-anecdotes/src/services/anecdotes.js
@@ -24,4 +24,18 @@ const create = async (content) => {
   return await response.json();
 };
 
-export default { getAll, create };
+const update = async (id, anecdote) => {
+  const response = await fetch(`${url}/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(anecdote),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to update anecdote");
+  }
+
+  return await response.json();
+};
+
+export default { getAll, create, update };
--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -1,18 +1,23 @@
 import { create } from "zustand";
 import anecdotesService from "./services/anecdotes";
 
-const useAnecdoteStore = create((set) => ({
+const useAnecdoteStore = create((set, get) => ({
   anecdotes: [],
   filter: "",
   actions: {
-    vote: (id) =>
+    vote: async (id) => {
+      const anecdote = get().anecdotes.find((anecdote) => anecdote.id === id);
+      const updatedAnecdote = await anecdotesService.update(id, {
+        ...anecdote,
+        votes: anecdote.votes + 1,
+      });
+
       set((state) => ({
         anecdotes: state.anecdotes.map((anecdote) =>
-          anecdote.id === id
-            ? { ...anecdote, votes: anecdote.votes + 1 }
-            : anecdote,
+          anecdote.id !== id ? anecdote : updatedAnecdote,
         ),
-      })),
+      }));
+    },
     add: async (content) => {
       const anecdote = await anecdotesService.create(content);
       set((state) => ({ anecdotes: state.anecdotes.concat(anecdote) }));
