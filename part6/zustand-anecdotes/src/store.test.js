c42d3c24d54b51b1b70fccf313e5d934ae432b68
Luiz Lopes <luizclopes.soares@gmail.com>
2026-04-30 07:49:41 -0300

    6.14 Anecdotes, step13

--- part6/zustand-anecdotes/src/store.test.js
+++ part6/zustand-anecdotes/src/store.test.js
@@ -66,3 +66,40 @@ describe("useAnecdoteActions", () => {
     expect(anecdotesResult.current).toEqual(sortedAnecdotes);
   });
 });
+
+describe("useAnecdotes filtering", () => {
+  const anecdotes = [
+    {
+      content: "If it hurts, do it more often",
+      id: "47145",
+      votes: 0,
+    },
+    {
+      content: "Adding manpower to a late software project makes it later!",
+      id: "21149",
+      votes: 1,
+    },
+    {
+      content:
+        "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      id: "69581",
+      votes: 2,
+    },
+  ];
+
+  beforeEach(() => {
+    useAnecdoteStore.setState({ anecdotes });
+  });
+
+  it("Return all anecdotes with no filter", () => {
+    const { result } = renderHook(() => useAnecdotes());
+    expect(result.current).toHaveLength(3);
+  });
+
+  it("Filter important anecdotes", () => {
+    useAnecdoteStore.setState({ anecdotes, filter: "hurts" });
+    const { result } = renderHook(() => useAnecdotes());
+
+    expect(result.current).toEqual([anecdotes[0]]);
+  });
+});
